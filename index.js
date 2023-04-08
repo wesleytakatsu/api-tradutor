3851e811ce157bd43830e70f20f488b835700df7
Wesley Sieiro Takatsu de Araujo <wesleytakatsu2@gmail.com>
2023-04-08 09:17:58 -0300

    Retirando logs desnecessarios

--- index.js
+++ index.js
@@ -15,13 +15,18 @@ const apiGoogleTranslate = async function(detect_linguage, translate_to, text){
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
+  console.log("Dados recebidos: detect_linguage: "+detect_linguage+", translate_to: "+translate_to+", text: "+text)
+
   var url = `https://translate.google.com.br/?sl=${detect_linguage}&tl=${translate_to}&text=${encodeURIComponent(text)}&op=translate`;
+
+  console.log("URL Gerada: "+url)
 
   await page.goto(url)
   await page.waitForTimeout(5000);
   
   const result = await page.evaluate(() => {
     //  MANTER ATENTO NO GOOGLE POIS ELES MUDAM A CLASSE DE TEMPOS EM TEMPOS
+    console.log(document.getElementsByClassName('HwtZe')[0].innerText)
     return document.getElementsByClassName('HwtZe')[0].innerText;
   })
 
